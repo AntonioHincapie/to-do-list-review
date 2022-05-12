66fd23efb384bf1ee7f856207fdac3d574a9423e
AntonioHincapie <marcohincapiem@gmail.com>
2022-05-12 12:26:54 -0500

    Make DRY my code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// import _ from 'lodash';
-// import './style.css';
 import { clean, refresh } from './modules/variables.js';
 import printTasks from './modules/printtasks.js';
 import clearAll from './modules/cleancompleted.js';
--- modules/variables.js
+++ modules/variables.js
@@ -1,5 +1,4 @@
 export const refresh = document.getElementById('reload');
-// export const addTarea = document.getElementById('add');
 export const addInput = document.getElementById('addinput');
 export const submitTask = document.getElementById('submittask');
 export const list = document.getElementById('list');
