27167fa71bf03b16eff564eb9697d8ed5c3ccfa0
AntonioHincapie <marcohincapiem@gmail.com>
2022-05-12 11:30:28 -0500

    Link modules and stylesheet

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // import _ from 'lodash';
-import './style.css';
+// import './style.css';
 import { clean, refresh } from './modules/variables.js';
 import printTasks from './modules/printtasks.js';
 import clearAll from './modules/cleancompleted.js';
